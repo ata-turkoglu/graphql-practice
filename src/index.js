d92378a6859a65944b51a6baa4c8943874814c86
ata_turkoglu <ata.turkoglu@reengen.com>
2023-12-14 23:44:46 +0300

    add create mutations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
+import { v4 as uuidv4 } from "uuid";
 
 const users = [
     {
@@ -90,6 +91,12 @@ const typeDefs = `
         comments: [Comment!]!
     }
 
+    type Mutation {
+        createUser (name: String!, email: String!, age: Int): User!
+        createPost (title: String!, body: String!, published: Boolean!, author: ID!): Post!
+        createComment (text: String!, author: ID!, post: ID!): Comment!
+    }
+
     type User {
         id: ID!
         name: String!
@@ -143,6 +150,54 @@ const resolvers = {
         },
         comments(parent, args, context, info) {
             return comments;
+        },
+    },
+    Mutation: {
+        createUser(parent, args, context, info) {
+            const emailTaken = users.some((user) => user.email == args.email);
+            if (emailTaken) throw new Error("Email taken.");
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age,
+            };
+            console.log("user", user);
+            users.push(user);
+            return user;
+        },
+        createPost(parent, args, context, info) {
+            const userExists = users.find((user) => user.id == args.author);
+            if (!userExists) throw new Error("User not found.");
+            const post = {
+                id: uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author,
+            };
+            posts.push(post);
+            return post;
+        },
+        createComment(parent, args, context, info) {
+            const userExists = users.find((user) => user.id == args.author);
+            if (!userExists) throw new Error("User not found.");
+
+            const postExists = posts.find(
+                (post) => post.id == args.post && post.published
+            );
+            if (!postExists) throw new Error("Post not found.");
+
+            const comment = {
+                id: uuidv4(),
+                text: args.text,
+                author: args.author,
+                post: args.post,
+            };
+
+            comments.push(comment);
+            return comment;
         },
     },
     Post: {
